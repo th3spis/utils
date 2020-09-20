2d10c25af516db00b516049ca670dbe8c1fd1e80
th3sp1s <69475918+th3sp1s@users.noreply.github.com>
2020-09-20 19:22:16 +0200

    Update curlList.sh

--- curlList.sh
+++ curlList.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+
+#    -o /dev/null throws away the usual output
+#    --silent throws away the progress meter
+#    --head makes a HEAD HTTP request, instead of GET
+#    --write-out '%{http_code}\n' prints the required status code
+# If too many lines in url-list, better run in background 
+
 while read LINE; do
   curl -o /dev/null --silent --head --write-out "%{http_code} $LINE\n" "$LINE"
 done < url-list.txt
