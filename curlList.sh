daf0a2ef1636ba2c4270a83cef2665f140ee75a8
th3sp1s <69475918+th3sp1s@users.noreply.github.com>
2020-09-20 19:25:59 +0200

    Update curlList.sh

--- curlList.sh
+++ curlList.sh
@@ -6,6 +6,7 @@
 #    --head makes a HEAD HTTP request, instead of GET
 #    --write-out '%{http_code}\n' prints the required status code
 # If too many lines in url-list, better run in background 
+# Not a bad idea to set a timeout with " --max-time 5.5 " for 5.5 secods, for example
 
 while read LINE; do
   curl -o /dev/null --silent --head --write-out "%{http_code} $LINE\n" "$LINE"
