f93aa4b4097a2f1c4d48f481ad8a6e0f3b21937f
th3sp1s <69475918+th3sp1s@users.noreply.github.com>
2021-02-23 18:19:38 +0100

    Update ipchecker.sh

--- ipchecker.sh
+++ ipchecker.sh
@@ -5,5 +5,20 @@ do
 	url=https://www.abuseipdb.com/check/$line
 	#curl writes the output to stderr, so redirect that and also suppress the progress to grep the html
 	#sed cleans html tags
-	curl --silent  $url 2>&1 | grep "was first reported on" | sed -e 's/<[^>]*>//g'
+	#curl --silent  $url 2>&1 | grep "was first reported on" | sed -e 's/<[^>]*>//g'
+	
+	match1=$(curl --silent $url | grep "This IP was reported" | sed -e 's/<[^>]*>//g' )
+	#Check the 
+	if [ ${#match1} -gt 1 ]
+	then
+		#He tenido que separar las siguientes dos queries porque se hacían de forma asíncrona automáticamente
+		#ineficiente de cojones 3 peticiones lo sé, pero tengo prisa
+		match2=$(curl --silent $url | grep "distinct source" | sed -e 's/<[^>]*>//g' )
+		match3=$(curl --silent $url | grep -E "first reported|most recently reported" | sed -e 's/<[^>]*>//g' )
+		echo "$line:"
+		echo "$match1"
+		echo "$match2"
+		echo "$match3"
+		echo
+	fi
 done<$1
